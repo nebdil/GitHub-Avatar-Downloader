491c9b8c2afb2f417f61394dc0efa3944045619e
Dilan <dilannebioglu@gmail.com>
2017-10-09 21:56:33 +0000

    fixed the user-agent header issue and printed the body

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,23 @@ var request = require('request');
 var GITHUB_USER = "nebdil";
 var GITHUB_TOKEN = "f22994992b965f0ee2fe38fd77c1cf62a5135ba6";
 console.log('Welcome to the GitHub Avatar Downloader!');
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request(options, function(err, response, body) {
+    if (err) throw err;
+    console.log(response.statusCode);
+    console.log(body);
+  });
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+var url = getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
