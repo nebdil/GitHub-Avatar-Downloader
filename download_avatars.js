4ccb1b8519d46bd9201f19fd222b59ffc2332685
Dilan <dilannebioglu@gmail.com>
2017-10-09 20:46:24 +0000

    added the URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,10 @@
 var request = require('request');
+var GITHUB_USER = "nebdil";
+var GITHUB_TOKEN = "f22994992b965f0ee2fe38fd77c1cf62a5135ba6";
 console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
