3c1cb862836bb914eab1e14c76629689fbd24cc1
Dilan <dilannebioglu@gmail.com>
2017-10-09 22:45:43 +0000

    created the downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "nebdil";
 var GITHUB_TOKEN = "f22994992b965f0ee2fe38fd77c1cf62a5135ba6";
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -17,9 +18,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var parsedArr = JSON.parse(body);
     // console.log(parsedObj);
     cb(err, parsedArr);
-
   });
 }
+function downloadImageByURL(url, filePath) {
+  request.get(url).on('error', function(err) {
+    throw err;
+  }).pipe(fs.createWriteStream(filePath));
+};
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
